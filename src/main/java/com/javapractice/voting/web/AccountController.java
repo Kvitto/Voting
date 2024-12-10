34c0d5b51bad3d0af49801c725d067cecfe530c3
Kvitto <kian04@mail.ru>
2024-12-10 14:01:57 +0300

    db_security_auth_user

--- src/main/java/com/javapractice/voting/web/AccountController.java
+++ src/main/java/com/javapractice/voting/web/AccountController.java
@@ -1,6 +1,9 @@
 package com.javapractice.voting.web;
 
+import com.javapractice.voting.AuthUser;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/account")
 public class AccountController {
 
-    public Object get(@AuthenticationPrincipal Object authUser) {
-        return authUser;
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object get(@AuthenticationPrincipal AuthUser authUser) {
+        return authUser.getUser();
     }
 }
